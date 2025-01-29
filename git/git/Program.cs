d7b2f3cb3315dd06607642227e02200a54a7cac5
DarynaCherkavska <vt232_chdv@student.ztu.edu.ua>
2025-01-29 23:08:01 +0200

    second commit

---
+++ git/git/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Введіть перше число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+
+            Console.WriteLine($"Сума чисел {num1} + {num2} = {sum}");
+        }
+    }
+}
